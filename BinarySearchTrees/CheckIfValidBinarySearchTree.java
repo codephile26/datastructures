3281f2b10acdc964a5c2e6d31daccb6dccd1190c
codephile26 <dimples2609@gmail.com>
2018-01-29 23:46:29 +0530

    Adding all approaches to check if a binary tree is a valid binary search tree or not

--- BinarySearchTrees/CheckIfValidBinarySearchTree.java
+++ BinarySearchTrees/CheckIfValidBinarySearchTree.java
@@ -2,23 +2,72 @@ package edu.ds.binarysearchtrees;
 
 public class CheckIfValidBinarySearchTree {
 	private static int prev = Integer.MIN_VALUE;
-	
-	public static boolean checkIfValidBSTInorder(BinarySearchTreeNode root){
-		return isBST(root);
+
+	public static boolean checkIfValidBSTInorder(BinarySearchTreeNode root) {
+		return checkIfValidBST(root);
 	}
 
-	private static boolean isBST(BinarySearchTreeNode root) {
-		if (root == null){
+	private static boolean checkIfValidBST(BinarySearchTreeNode root) {
+		if (root == null) {
 			return true;
 		}
-		if (!isBST(root.getLeft())){
+		if (!checkIfValidBST(root.getLeft())) {
 			return false;
 		}
-		if (root.getData() < prev){
+		if (root.getData() < prev) {
 			return false;
 		}
 		prev = root.getData();
-		return isBST(root.getRight());
+		return checkIfValidBST(root.getRight());
+	}
+
+	public static boolean checkIfValidBST(BinarySearchTreeNode root, int min, int max) {
+		if (root == null) {
+			return true;
+		}
+
+		return ((root.getData() > min && root.getData() < max) && checkIfValidBST(root.getLeft(), min, root.getData())
+				&& checkIfValidBST(root.getRight(), root.getData(), max));
+	}
+	
+	public static boolean checkIfValidBSTMaxMin(BinarySearchTreeNode root){
+		if (root == null){
+			return true;
+		}
+		
+		if (root.getLeft() != null && findMaximum(root.getLeft()).getData() > root.getData()){
+			return false;
+		}
+		
+		if (root.getRight() != null && findMinimum(root.getRight()).getData() < root.getData()){
+			return false;
+		}
+		
+		if (!checkIfValidBSTMaxMin(root.getLeft()) || !checkIfValidBSTMaxMin(root.getRight())){
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static BinarySearchTreeNode findMinimum(BinarySearchTreeNode root) {
+		if (root == null) {
+			return null;
+		} else if (root.getLeft() == null) {
+			return root;
+		} else {
+			return findMinimum(root.getLeft());
+		}
+	}
+
+	public static BinarySearchTreeNode findMaximum(BinarySearchTreeNode root) {
+		if (root == null) {
+			return null;
+		} else if (root.getRight() == null) {
+			return root;
+		} else {
+			return findMaximum(root.getRight());
+		}
 	}
 
 }
