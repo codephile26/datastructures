9ab58cdfac65498d51881461bb969ff063ea5936
Dimple Shah <dimples2609@gmail.com>
2018-02-08 22:58:18 +0530

    added the implementation of DFS in an undirected graph

--- Graphs/Graph.java
+++ Graphs/Graph.java
@@ -1,36 +1,64 @@
 package edu.ds.graphs;
 
+import java.util.Stack;
+
 public class Graph {
-	
-	private boolean adjMatrix[][];
+	private final int maxVertices = 20;
+	private Vertex vertexList[];
+	private int adjMatrix[][];
 	private int vertexCount;
 	
-	public Graph(int vertexCount){
-		this.vertexCount = vertexCount;
-		adjMatrix = new boolean[vertexCount][vertexCount];
+	public Graph() {
+		//used to number the vertices - as they are added in a graph
+		vertexList = new Vertex[maxVertices];
+		//used to denote edges in between any two vertices in the graph.
+		adjMatrix = new int[maxVertices][maxVertices];
+		//used to keep track of the number of vertices added so far.
+		vertexCount = 0;
+	}
+	
+	public void addVertex(char label){
+		vertexList[vertexCount++] = new Vertex(label);
 	}
 	
 	public void addEdge(int i, int j){
-		if (i >= 0 && i < vertexCount && j >=0 && j < vertexCount){
-			adjMatrix[i][j] = true;
-			adjMatrix[j][i] = true;
+		if (i >= 0 && i < maxVertices && j >=0 && j < maxVertices){
+			adjMatrix[i][j] = 1;
+			adjMatrix[j][i] = 1;
 		}
 	}
 	
-	public void removeEdge(int i,int j){
-		if (i >= 0 && i < vertexCount && j >= 0 && j < vertexCount){
-			adjMatrix[i][j] = false;
-			adjMatrix[j][i] = false;
-		}
+	//method to display vth vertex in a graph
+	public void displayVertex(int v){
+		System.out.print(vertexList[v].getLabel() + " ");
 	}
 	
-	public boolean isEdge(int i,int j){
-		if (i >= 0 && i < vertexCount && j >= 0 && j < vertexCount){
-			return adjMatrix[i][j];
+	public void dfs(){
+		Stack<Integer> stack = new Stack<Integer>();
+		vertexList[0].setVisited(true);
+		displayVertex(0);
+		stack.push(0);
+		while (!stack.isEmpty()){
+			//get an unvisited adjacent vertex to stack top
+			int v = getAdjUnvisitedVertex(stack.peek());
+			if (v == -1){
+				stack.pop();
+			}
+			else {
+				vertexList[v].setVisited(true);
+				displayVertex(v);
+				stack.push(v);
+			}
 		}
-		else return false;
 	}
-	
-	
+
+	private int getAdjUnvisitedVertex(int peek) {
+		for (int j=0; j < vertexCount;j++){
+			if (adjMatrix[peek][j] == 1 && vertexList[j].isVisited() == false){
+				return j;
+			}
+		}
+		return -1;
+	}
 	
 }
