3a7736015bd59bc6ad138b832ee4c2787a1186cc
codephile26 <dimples2609@gmail.com>
2018-02-08 23:30:15 +0530

    Implemented test case for bfs traversal of a graph

--- Graphs/GraphDfsTest.java
+++ Graphs/GraphDfsTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class GraphDfsTest {
 
 	@Test
-	public void test() {
+	public void testDfs() {
 		Graph g = new Graph();
 		g.addVertex('A');
 		g.addVertex('B');
@@ -26,6 +26,29 @@ public class GraphDfsTest {
 //		g.addEdge(4, 6);
 		g.addEdge(4, 7);
 		g.dfs();
+		
+	}
+	
+	@Test
+	public void testBfs() {
+		Graph g = new Graph();
+		g.addVertex('A');
+		g.addVertex('B');
+		g.addVertex('C');
+		g.addVertex('D');
+		g.addVertex('E');
+		g.addVertex('F');
+		g.addVertex('G');
+		g.addVertex('H');
+		g.addEdge(0, 1);
+		g.addEdge(1, 2);
+		g.addEdge(1, 7);
+		g.addEdge(2, 3);
+		g.addEdge(2, 4);
+		g.addEdge(4, 5);
+		g.addEdge(4, 7);
+		g.addEdge(4, 6);
+		g.bfs();
 		
 	}
 
